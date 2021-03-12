55709301e84c487d2e84b5fb12f30d48349c3e08
Laii Lê <lelaii200398@gmail.com>
2021-03-12 13:38:34 +0700

    Revert "Complete User Layout"
    This reverts commit ab065e1722bf35d12e33333e67832b123b5fcfe4.

--- WebsiteTuDien/App_Start/RouteConfig.cs
+++ WebsiteTuDien/App_Start/RouteConfig.cs
@@ -14,15 +14,9 @@ namespace WebsiteTuDien
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "SiteSlug",
-                url: "{slug}",
-                defaults: new { controller = "Site", action = "Home", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Site", action = "Home", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
