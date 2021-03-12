ab065e1722bf35d12e33333e67832b123b5fcfe4
Laii Lê <lelaii200398@gmail.com>
2021-03-12 13:38:17 +0700

    Complete User Layout

--- WebsiteTuDien/App_Start/RouteConfig.cs
+++ WebsiteTuDien/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace WebsiteTuDien
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "SiteSlug",
+                url: "{slug}",
+                defaults: new { controller = "Site", action = "Home", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Site", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
